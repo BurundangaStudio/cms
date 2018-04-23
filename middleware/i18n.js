3bae6b5f2607d1af9fae86da040030289b22d1bc
Christian MacMillan <christian.macmillan@b-reel.com>
2018-04-23 10:36:00 +0200

    handles language redirect when route param lang error

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -6,7 +6,6 @@
 // April 2018 | http://burundanga.studio
 
 export default function({ isHMR, app, store, route, params, redirect }) {
-
     const defaultLocale = app.i18n.fallbackLocale;
 
     if (isHMR) return;
@@ -17,27 +16,26 @@ export default function({ isHMR, app, store, route, params, redirect }) {
 
     app.i18n.locale = store.state.lang.locale;
 
-    // if (store.state.lang.locales.indexOf(locale) === -1 || !params.lang) {
+    if (store.state.lang.locales.indexOf(locale) === -1 || !params.lang) {
 
-    //     let routeParts = route.fullPath.split("/");
-    //     let redir = "/";
+        let routeParts = cleanArrayOf(route.fullPath.split("/"));
+        let redir = "/";
 
-    //     if (!params.lang) {
-    //         console.log("entro")
-    //         redir += defaultLocale + route.fullPath
-    //     } else {
-    //         redir = getSameRouteWithLang(routeParts, defaultLocale);
-    //     }
-    //     console.log(redir);
-    //     // return redirect(redir);
-    // }
+        if (params.lang) routeParts.splice(0, 1);
+
+        redir += defaultLocale;
+        Array.from(routeParts).forEach(part => {
+            redir += "/" + part;
+        });
+
+        return redirect(redir);
+    }
 }
 
-function getSameRouteWithLang(routeParts, lang, noLang = false) {
-    if (!noLang) routeParts.splice(0, 2);
-    let route = "/" + lang;
-    Array.from(routeParts).forEach((routePart, index) => {
-        route += ((noLang && index == 0) ? "" : "/") + routePart;
-    })
-    return route;
+function cleanArrayOf(array) {
+    const newArray = [];
+    Array.from(array).forEach(part => {
+        if (part != "") newArray.push(part);
+    });
+    return newArray;
 }
