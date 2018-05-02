1f4bbfdefeebf57b35c9a1efeccc19ecfef28065
Christian MacMillan <christian.macmillan@b-reel.com>
2018-05-02 13:44:23 +0200

    database setted wip

--- plugins/database.js
+++ plugins/database.js
@@ -13,16 +13,32 @@ const firestore = require("firebase/firestore");
 const _db = firebase.firestore();
 const _files = fs.readdirSync("config/database");
 
+let refs = [];
+
 Array.from(_files).forEach(_file => {
     const _json = require("../config/database/" + _file);
     const _collection = _file.replace(".json", "");
     for (var _doc in _json) {
         const ref = _collection + "/" + _doc;
-        const _docRef = _db
-            .doc(ref)
-            .get()
-            .then(snap => {
-                console.log(ref + " exists: " + snap.exists);
-            });
+        refs.push(ref);
     }
 });
+
+setRef(0);
+
+function setRef(i) {
+    const ref = refs[i];
+    _db
+        .doc(ref)
+        .get()
+        .then(snap => {
+            console.log(ref + " snapexists: " + snap.exists);
+            if (i < refs.length - 1) {
+                i++;
+                setRef(i);
+            } else {
+                console.log("\nDATABASE SETTED! \n");
+                process.exit();
+            }
+        });
+}
